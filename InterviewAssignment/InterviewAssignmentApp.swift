9a2e5ba152be027fff9eee4f1a6207689990e9ee
avalonwalraven <111320543+avalonwalraven@users.noreply.github.com>
2023-11-15 17:31:43 -0700

    Initial Commit

---
+++ InterviewAssignment/InterviewAssignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  InterviewAssignmentApp.swift
+//  InterviewAssignment
+//
+//  Created by Avalon Walraven on 11/15/23.
+//
+
+import SwiftUI
+
+@main
+struct InterviewAssignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
